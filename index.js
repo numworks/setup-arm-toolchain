93229aee315aec8baa4d60772bd7a86bf1e75d9e
Romain Goyet <romain.goyet@numworks.com>
2020-11-17 14:44:40 -0500

    Update the README

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 async function run() {
   try {
-    const downloadUrl = "https://github.com/numworks/setup-arm-toolchain/releases/download/2019-q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2";
+    const downloadUrl = "https://github.com/numworks/setup-arm-toolchain/releases/download/2020-q2-assets/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2";
     const gccTarPath = await tc.downloadTool(downloadUrl);
     const gccPath = await tc.extractTar(gccTarPath, null, 'xj');
     const binPath = path.join(gccPath, 'gcc-arm-none-eabi-9-2019-q4-major', 'bin');
