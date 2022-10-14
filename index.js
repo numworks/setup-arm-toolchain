91b5b7a7177e5e59fe88c5d49fb35a925474f45b
Romain Goyet <romain.goyet@numworks.com>
2022-10-14 11:31:25 -0400

    Use 2022-08

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 async function run() {
   try {
     // Change the three following constants when updating GCC version
-    const downloadUrl = 'https://github.com/numworks/setup-arm-toolchain/releases/download/2020-q4-assets/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2';
-    const versionTag = '2020.4.0'; // tool-cache expects this to follow semver.org
-    const binPath = ['gcc-arm-none-eabi-10-2020-q4-major', 'bin']; // Path to the binary folder in the tarball
+    const downloadUrl = 'https://github.com/numworks/setup-arm-toolchain/releases/download/2022-08-assets/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz';
+    const versionTag = '2022.8.0'; // tool-cache expects this to follow semver.org
+    const binPath = ['arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi', 'bin']; // Path to the binary folder in the tarball
 
     const cacheKey = 'gcc-arm-none-eabi';
     let cachedGCCPath = tc.find(cacheKey, versionTag);
@@ -17,7 +17,7 @@ async function run() {
       core.info(`Downloading version ${cacheKey} from ${downloadUrl}`);
       const downloadGCCTarPath = await tc.downloadTool(downloadUrl);
       core.info(`Extracting version ${cacheKey}`);
-      const downloadGCCPath = await tc.extractTar(downloadGCCTarPath, null, 'xj');
+      const downloadGCCPath = await tc.extractTar(downloadGCCTarPath, null, 'xJ');
       core.info(`Caching version ${cacheKey}`);
       cachedGCCPath = await tc.cacheDir(downloadGCCPath, cacheKey, versionTag);
     }
