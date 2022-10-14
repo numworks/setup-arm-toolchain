c1f0e21e1138a5e826ce48f58a62870f86f34cf0
Romain Goyet <romain.goyet@numworks.com>
2022-10-14 11:48:58 -0400

    2020-q2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 async function run() {
   try {
     // Change the three following constants when updating GCC version
-    const downloadUrl = 'https://github.com/numworks/setup-arm-toolchain/releases/download/2020-q4-assets/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2';
-    const versionTag = '2020.4.0'; // tool-cache expects this to follow semver.org
-    const binPath = ['gcc-arm-none-eabi-10-2020-q4-major', 'bin']; // Path to the binary folder in the tarball
+    const downloadUrl = 'https://github.com/numworks/setup-arm-toolchain/releases/download/2020-q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2';
+    const versionTag = '2020.2.0'; // tool-cache expects this to follow semver.org
+    const binPath = ['gcc-arm-none-eabi-9-2020-q2-update', 'bin']; // Path to the binary folder in the tarball
 
     const cacheKey = 'gcc-arm-none-eabi';
     let cachedGCCPath = tc.find(cacheKey, versionTag);
